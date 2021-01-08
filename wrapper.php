08fc97c5ce565b17592887a1d77db1d2a90f3d36
altendorfme <altendorfme@gmail.com>
2021-01-08 19:44:08 -0300

    local countries compare

--- wrapper.php
+++ wrapper.php
@@ -10,7 +10,8 @@ $source_array = array_map("str_getcsv", explode("\n", $source));
 unset($source_array[count($source_array)-1]);
 array_splice($source_array, 0, 1);
 
-$countries_array = json_decode(file_get_contents('https://raw.githubusercontent.com/stefangabos/world_countries/master/data/pt/countries.json'));
+// https://raw.githubusercontent.com/stefangabos/world_countries/master/data/pt/countries.json
+$countries_array = json_decode(file_get_contents('countries.json'));
 if($countries_array === FALSE) {
     http_response_code(404);
     die();
